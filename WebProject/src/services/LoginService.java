60ec6f80d8ee8b99229f776f7ed0e0832ba92724
djordjekrsmanovic <djordje1499@gmail.com>
2021-06-24 15:19:29 +0200

    dodate kolegine stavke

---
+++ WebProject/src/services/LoginService.java
@@ -0,0 +1,26 @@
+package services;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/login")
+public class LoginService {
+
+	@POST
+	@Path("")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String loginTry() {
+
+		return "Uspjesno logovanje!";
+	}
+
+	@GET
+	@Path("/registracija")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test() {
+		System.out.println("test");
+		return "OK";
+	}
+}
